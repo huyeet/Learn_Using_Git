54624dfebb01954bef64785aa8349240d9fb8d88
huyeet <hnguyen21@pickeringcollege.on.ca>
2020-12-08 18:17:42 -0500

    Day 8: Brute force second half....

--- Advent_Of_Code/day_eight.py
+++ Advent_Of_Code/day_eight.py
@@ -1,5 +1,6 @@
 # You will have to look into other people's solutions after you've done with this.
 # Try to not spend too much time on this as you have a test tomorrow.
+# Part two: Try to run the commands backwards...
 from pathlib import Path
 import time
 import regex
@@ -17,25 +18,77 @@ def total_before_inf(accumulator: int, instructions: list):
     PATTERN = r"(acc|jmp|nop) ([+-]\d*)"
     flag_list = [0 for i in range(len(instructions))]
     index = 0
-    while flag_list[index] != 2:
+    instructions_index = []
+    # Original break condition: flag_list[index] != 2
+    while flag_list[index] != 2 and index < len(instructions):
         instruction_match = regex.match(PATTERN, instructions[index])
         type = instruction_match.group(1)
         value = int(instruction_match.group(2))
+        #print(f"{index}: {type}: {value} - accumulator before op: {accumulator}")
         if type == "acc":
             accumulator += value
             index += 1
         elif type == "nop":
             index += 1
+            instructions_index.append(index)
         elif type == "jmp":
             index += value
+            instructions_index.append(index)
         flag_list[index] += 1
-        print(accumulator)
+        #print(f"Future instruction: {index}: {instructions[index]}")
+    return instructions_index
+
+
+def change_command(instructions_index: list, all_instructions: list):
+    # Goal, if backwards encounter infinite loops, change the command!
+    # Second alternative: Detect the one causing infinite loops through running chronologically.
+    # Preferably through checking the first jmp having a 2 value flag, then backpedal it.
+    # Yeah, have  a list of index number, then backpedal the damn thing.
+    accumulator = 0
+    index = 0
+    flag_list = [0 for i in range(len(all_instructions))]
+    PATTERN = r"(acc|jmp|nop) ([+-]\d*)"
+
+    # Brute force incoming...
+    for instruction in instructions_index:
+        while index < len(all_instructions):
+            instruction_match = regex.match(PATTERN, all_instructions[index])
+            type = instruction_match.group(1)
+            value = int(instruction_match.group(2))
+            if type == "acc":
+                accumulator += value
+                index += 1
+            elif type == "nop":
+                if all_instructions[index] == all_instructions[instruction]:
+                    print(f"{instruction}: {all_instructions[index]}")
+                    index += value
+                else:
+                    index += 1
+            elif type == "jmp":
+                if all_instructions[index] != all_instructions[instruction]:
+                    index += value
+                else:
+                    print(f"{instruction}: {all_instructions[index]}")
+                    index += 1
+            if index < len(all_instructions):
+                flag_list[index] += 1
+                if flag_list[index] == 2:
+                    break
+            else:
+                break
+        if index == len(all_instructions):
+            break
+        else:
+            accumulator = 0
+            index = 0
+            flag_list = [0 for i in range(len(all_instructions))]
     return accumulator
 
 
 start_time = time.time()
 all_instructions = receive_batch(PATH)
 boom = total_before_inf(accumulator, all_instructions)
-print(boom)
+change_one_command = change_command(instructions_index=boom, all_instructions=all_instructions)
+print(change_one_command)
 print(f"--- {time.time() - start_time} seconds ---")
 
